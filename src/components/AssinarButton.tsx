0595f59b41f7a56dde2be8d0dd14ea26f47fdcca
GuilhermeUberti <guilherme.uberti@gmail.com>
2025-07-17 11:06:43 -0300

    checkout4

--- src/components/AssinarButton.tsx
+++ src/components/AssinarButton.tsx
@@ -13,7 +13,7 @@ export default function AssinarButton() {
     setLoading(true)
 
     try {
-      const response = await fetch("https://giftplay-backend.onrender.com/checkout", {
+      const response = await fetch("https://giftplay-backend.onrender.com/api/checkout", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
